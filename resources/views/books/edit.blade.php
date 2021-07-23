baa9242426130fbffff4c2913f9ee79aeefb26a6
Ferlyn Ruiz <ruizferlyn133@gmail.com>
2021-07-23 14:51:12 +0800

    Initial commit

---
+++ resources/views/books/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('books.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Book Details</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('books.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('books.update',$book->id) }}" method="POST">
+
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Title:</strong>
+                    <input type="text" name="title" value="{{ $book->title }}" class="form-control" placeholder="Title">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Author:</strong>
+                    <input type="text" name="author" value="{{ $book->author }}" class="form-control" placeholder="Author">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Publisher:</strong>
+                    <input type="text" name="publisher" value="{{ $book->publisher }}" class="form-control" placeholder="Publisher">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Genre:</strong>
+                    <input type="text" name="genre" value="{{ $book->genre }}" class="form-control" placeholder="Genre">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Synopsis:</strong>
+                    <textarea class="form-control" style="height:150px" name="synopsis" placeholder="Synopsis">{{ $book->synopsis }}</textarea>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,51 @@
+@extends('books.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Book Details</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('books.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                {{ $book->title }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Author:</strong>
+                {{ $book->author }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Publisher:</strong>
+                {{ $book->publisher }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Genre:</strong>
+                {{ $book->genre }}
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Synopsis:</strong>
+                {{ $book->synopsis }}
+            </div>
+        </div>
+    </div>
+@endsection
